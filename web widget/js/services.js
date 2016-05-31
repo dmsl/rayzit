aa09846a639903aa23723c0f692095bc6801a7c7
dstefa02 <dsteaf02@cs.ucy.ac.cy>
2016-05-31 18:12:31 +0300

    Web widget First Commit

--- web widget/js/services.js
+++ web widget/js/services.js
@@ -1,10 +1,32 @@
+/**
+ *
+ The MIT License (MIT)
+ Copyright (c) 2016, Dimosthenis Stefanidis, Data Management Systems Laboratory (DMSL)
+ Department of Computer Science, University of Cyprus, Nicosia, CYPRUS,
+ dmsl@cs.ucy.ac.cy, http://dmsl.cs.ucy.ac.cy/
+ Permission is hereby granted, free of charge, to any person obtaining a copy
+ of this software and associated documentation files (the "Software"), to deal
+ in the Software without restriction, including without limitation the rights
+ to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ copies of the Software, and to permit persons to whom the Software is
+ furnished to do so, subject to the following conditions:
+ The above copyright notice and this permission notice shall be included in
+ all copies or substantial portions of the Software.
+ THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ THE SOFTWARE.
+ */
 angular.module('ionicApp.services', [])
   .factory('sharedProperties', function ($http, $q, $timeout, $httpParamSerializerJQLike) {
      var LATITUDE;
      var LONGITUDE;
      var ACCURACY = "1000";
      var USER_ID;
-     var APP_ID = "EPL371_AVC3PAFGO054FLP3AGEL0L";
+     var APP_ID = "*************************";
      var STAR_PAGE_NUM = "1";
      var CUR_POWER = 0;
      var MAX_REQUESTS = 20;
