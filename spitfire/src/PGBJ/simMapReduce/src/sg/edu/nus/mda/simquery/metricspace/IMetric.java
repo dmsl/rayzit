e1da6dd010fa3f5fac115b304014c10aff7672c1
costantinos <costa.costantinos@gmail.com>
2016-10-27 12:28:29 +0300

    Remove PGBJ
    Remove the source code PGBJ.  H-NJ, H-BNLJ, H-BRJ, PGBJ Algorithms
    
    Please request the source code directly from: W. Lu, Y. Shen, S. Chen
    and B.C. Ooi. “Efficient processing of k nearest neighbor joins using
    mapreduce”. In Proceedings of the 38th international conference on Very
    Large Data Bases (PVLDB’12). VLDB Endowment 5, 1016–1027, 2012.

--- spitfire/src/PGBJ/simMapReduce/src/sg/edu/nus/mda/simquery/metricspace/IMetric.java
+++
@@ -1,9 +0,0 @@
-package sg.edu.nus.mda.simquery.metricspace;
-
-import java.io.IOException;
-
-public abstract class IMetric {
-	public long numOfDistComp = 0;
-	public abstract float dist(Object o1, Object o2)  throws IOException ;
-	public abstract long getNumOfDistComp();
-}
